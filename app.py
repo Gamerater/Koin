a1ab04c7b43283bf66431c00eefc9f42b1ffbf2c
Yuvraj <yuvrajsinghshekhawat62@gmail.com>
2026-03-24 10:35:59 +0530

    Fix: add flask-cors to resolve categories loadking issue

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import sqlite3
 from datetime import date
 
 app = Flask(__name__)
 
+CORS(app)
 def get_db():
     conn = sqlite3.connect("database/koin.db")
     conn.row_factory = sqlite3.Row #lets you access columns by names
